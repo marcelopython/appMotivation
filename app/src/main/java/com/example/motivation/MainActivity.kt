88000f77da6bde304d326b9bff6912414412fc4d
Marcelo <msr88957@gmail.com>
2024-08-07 13:04:10 -0300

    start project

---
+++ app/src/main/java/com/example/motivation/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.motivation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onClick(v: View?) {
+
+        println("passou por aqui")
+
+    }
+}
