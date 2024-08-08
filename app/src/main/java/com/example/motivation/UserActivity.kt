710aa69443cbba8c13f825a2a4a35073082d53cd
Marcelo <msr88957@gmail.com>
2024-08-07 22:15:02 -0300

    navegação entre activity

--- app/src/main/java/com/example/motivation/UserActivity.kt
+++ app/src/main/java/com/example/motivation/UserActivity.kt
@@ -1,9 +1,11 @@
 package com.example.motivation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.Toast
 import com.example.motivation.databinding.ActivityMainBinding
 import com.example.motivation.databinding.ActivityUserBinding
 
@@ -18,6 +20,7 @@ class UserActivity : AppCompatActivity(), View.OnClickListener {
         binding = ActivityUserBinding.inflate(layoutInflater);
         setContentView(binding.root)
 
+        supportActionBar?.hide()
 
         binding.btnSave.setOnClickListener(this)
 
@@ -25,7 +28,27 @@ class UserActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onClick(v: View) {
         if(v.id == R.id.btn_save){
-
+            handleSave()
         }
     }
+
+    private fun handleSave() {
+
+        val name = binding.editText.text.toString()
+
+        if(name != "") {
+
+            // Faz a criação e a navegação entre activitys
+            val intent: Intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+
+            //Destroi e tira da memoria essa tela apos a navegação
+            finish()
+
+        } else{
+            Toast.makeText(this, R.string.validation_mandatory_name, Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
 }
