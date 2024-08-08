7f7fedb1304f18d0de0040bb60ca5af6a0a5be21
Marcelo <msr88957@gmail.com>
2024-08-07 21:01:13 -0300

    set event click

--- app/src/main/java/com/example/motivation/MainActivity.kt
+++ app/src/main/java/com/example/motivation/MainActivity.kt
@@ -3,17 +3,28 @@ package com.example.motivation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.example.motivation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnNextText.setOnClickListener(this)
+
     }
 
-    override fun onClick(v: View?) {
+    override fun onClick(v: View) {
 
-        println("passou por aqui")
+        if(v.id == R.id.btn_next_text) {
+            println("passou por aqui")
+        }
 
     }
 }
